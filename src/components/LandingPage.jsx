ae9c58909c3b80f0339521d550d0002088370346
Mok <mok@moekm.com>
2024-09-10 22:36:35 +0300

    new VisualData component

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import Header from "./landing/Header.jsx";
+import VisualData from "./landing/VisualData.jsx";
 
 import "../styling/LandingPage.css";
 import placeholderIMG from "../../public/assets/images/placeholder.jpg";
@@ -66,8 +67,80 @@ export default function LandingPage() {
         </div>
       </section>
 
-      {/** Blueprint - will be replaced later with components */}
       <section className="dashboard-components">
+        <div id="container">
+          <VisualData id="alerts" type="custom-content-head">
+            <h3>Alerts - (3) Critical</h3>
+            <div id="visual-alerts">
+              <div id="graph" style={persentages}>
+                <div id="icon-text">
+                  <p>6</p>
+                  <p>Alerts</p>
+                </div>
+              </div>
+              <div id="icon-alerts">
+                <ul>
+                  <li>
+                    <svg
+                      width="30"
+                      height="27"
+                      viewBox="0 0 30 27"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.16 2.00952C12.707 -0.669842 16.5743 -0.669837 18.1212 2.00952L28.7368 20.3963C30.2837 23.0756 28.3501 26.4248 25.2562 26.4248H4.02503C0.931173 26.4248 -1.00248 23.0756 0.544446 20.3962L11.16 2.00952Z"
+                        fill="#E5383B"
+                      />
+                      <path
+                        d="M13.4416 18.8113L13.1574 7.16089H15.8571L15.5729 18.8113H13.4416ZM14.5783 23.8448C14.0889 23.8448 13.6863 23.6951 13.3706 23.3958C13.0706 23.0807 12.9206 22.7105 12.9206 22.2851C12.9206 21.844 13.0706 21.4738 13.3706 21.1744C13.6863 20.8593 14.0889 20.7018 14.5783 20.7018C15.0361 20.7018 15.415 20.8593 15.715 21.1744C16.0307 21.4738 16.1886 21.844 16.1886 22.2851C16.1886 22.7105 16.0307 23.0807 15.715 23.3958C15.415 23.6951 15.0361 23.8448 14.5783 23.8448Z"
+                        fill="white"
+                      />
+                    </svg>
+                    <span>(3) Critical</span>
+                  </li>
+                  <li>
+                    <svg
+                      width="29"
+                      height="11"
+                      viewBox="0 0 29 11"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect width="29" height="4" rx="2" fill="#F4B000" />
+                      <rect y="7" width="29" height="4" rx="2" fill="#F4B000" />
+                    </svg>
+                    <span>(2) Warnings</span>
+                  </li>
+                  <li>
+                    <svg
+                      width="29"
+                      height="11"
+                      viewBox="0 0 29 11"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        width="29"
+                        height="4"
+                        rx="2"
+                        fill="black"
+                        fillOpacity="0.15"
+                      />
+                      <rect y="7" width="29" height="4" rx="2" fill="#949494" />
+                    </svg>
+                    <span>(1) Other</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <p id="learn-more">
+              <button id="type-text">learn more {">"}</button>
+            </p>
+          </VisualData>
+
+          {/* 
+        KEEPT FOR LEGACY PURPOSES -- WILL REMOVE LATER
         <div className="component-container" id="alerts">
           <div id="content">
             <h3>Alerts - (3) Critical</h3>
@@ -138,9 +211,19 @@ export default function LandingPage() {
               <button id="type-text">learn more {">"}</button>
             </p>
           </div>
-        </div>
+        </div> */}
 
-        <div className="component-container" id="api-data">
+          <VisualData
+            id="api-data"
+            title={["API Summary", "Running APIs", "Offline APIs"]}
+            contentHead={["Total Cost:", "$200.38"]}
+            content={[
+              ["GOOGLE.COM", "20 requests", "$20"],
+              ["REMOVE-IMG.COM", "200 requests", "$312"],
+            ]}
+          />
+
+          {/* <div className="component-container" id="api-data">
           <div id="head">
             <div>
               <button id="type-text" className="active">
@@ -169,9 +252,21 @@ export default function LandingPage() {
               </li>
             </ul>
           </div>
-        </div>
+        </div> */}
 
-        <div className="component-container" id="account-overview">
+          <VisualData
+            id="account-overview"
+            title="Account Overview"
+            contentHead={["Jan 1 2024", "$200.38"]}
+            content={[
+              ["Total Running Containers:", "30"],
+              ["Total Completed Builds:", "33"],
+              ["Total Created Images:", "7"],
+              ["Total API requests:", "300"],
+            ]}
+          />
+
+          {/* <div className="component-container" id="account-overview">
           <div id="head">
             <div>
               <p>Account Overview</p>
@@ -201,9 +296,21 @@ export default function LandingPage() {
               </li>
             </ul>
           </div>
-        </div>
+        </div> */}
 
-        <div className="component-container" id="core-system">
+          <VisualData
+            id="core-system"
+            title="Core System Status"
+            content={[
+              ["CPU", "%10"],
+              ["GPU (1) ", "%43"],
+              ["Processes:", "20"],
+              ["Bandwidth:", "20GB"],
+              ["Uptime:", "2 Months"],
+            ]}
+          />
+
+          {/* <div className="component-container" id="core-system">
           <div id="head">
             <div>
               <p>Account Overview</p>
@@ -233,6 +340,7 @@ export default function LandingPage() {
               </li>
             </ul>
           </div>
+        </div> */}
         </div>
       </section>
     </>
---
+++ src/components/landing/VisualData.jsx
@@ -0,0 +1,72 @@
+export default function VisualData({
+  id,
+  type = "",
+  title,
+  contentHead = [],
+  content = [[]],
+  children,
+}) {
+  if (typeof type !== "string") {
+    throw Error(
+      "Passed an invalid 'type' prop to the component. of type: ",
+      typeof type
+    );
+  }
+
+  let isCustom = {
+    head: false,
+    content: false,
+  };
+
+  const btnNav = (typeof title == "object" && title.length) || false;
+  const strNav = typeof title == "string" ? true : false;
+
+  // the custom prefix "custom" is ignored and is there for better readablity
+  const typeArgs = type.split("-");
+  if (typeArgs.includes("content")) isCustom.content = true;
+  if (typeArgs.includes("head")) isCustom.head = true;
+
+  return (
+    <div className="component-container" id={id || null}>
+      {isCustom.head ? null : (
+        <div id="head">
+          <div>
+            {strNav && <p>{title}</p>}
+            {btnNav &&
+              title.map((btn) => {
+                return <button id="type-text">{btn}</button>;
+              })}
+          </div>
+        </div>
+      )}
+      <div id="content">
+        {isCustom.content ? (
+          children
+        ) : (
+          <>
+            {contentHead.length > 0 && (
+              <div id="content-head">
+                <div id="row">
+                  {contentHead.map((value) => (
+                    <p>{value}</p>
+                  ))}
+                </div>
+                <hr />
+              </div>
+            )}
+
+            <ul>
+              {content.map((item) => (
+                <li id="row">
+                  {item.map((value) => (
+                    <p>{value}</p>
+                  ))}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
