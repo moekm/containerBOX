ac5a38cab7bd6a7059d10c3a2c8bfc4b056a6da6
Mok <mok@moekm.com>
2024-09-24 14:39:36 +0300

    refactored

--- src/components/landing/VisualData.jsx
+++ src/components/landing/VisualData.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
+
 export default function VisualData({
   id,
   type = "",
   title,
+  tabs,
   contentHead = [],
   content = [[]],
   children,
@@ -13,64 +16,99 @@ export default function VisualData({
     );
   }
 
-  let isCustom = {
+  const isTabs = !tabs ? null : 0; // If tabs prop is defined, select index 0
+  const [activeTab, setActiveTab] = useState(isTabs);
+
+  function handleTabSwitch(index) {
+    setActiveTab(index);
+  }
+
+  let hasCustom = {
     head: false,
     content: false,
   };
 
+  // checks for the type of the head. string or button nav
   const btnNav = (typeof title == "object" && title.length) || false;
   const strNav = typeof title == "string" ? true : false;
 
   // the custom prefix "custom" is ignored and is there for better readablity
   const typeArgs = type.split("-");
-  if (typeArgs.includes("content")) isCustom.content = true;
-  if (typeArgs.includes("head")) isCustom.head = true;
+  if (typeArgs.includes("head")) hasCustom.head = true;
+  if (typeArgs.includes("content")) hasCustom.content = true;
+
+  // renders
+  const Head = () => {
+    return (
+      <div id="head">
+        <div>
+          {strNav && <p>{title}</p>}
+          {btnNav &&
+            title.map((btnText, i) => (
+              <button
+                key={i}
+                onClick={() => handleTabSwitch(i)}
+                id="type-text"
+                className={activeTab == i ? "active" : null}
+              >
+                {btnText}
+              </button>
+            ))}
+        </div>
+      </div>
+    );
+  };
+
+  const Content = ({ head, body }) => {
+    let headJSX;
+    if (head.length > 0) {
+      headJSX = (
+        <div id="content-head">
+          <div id="row">
+            {head.map((value, i) => (
+              <p key={i}>{value}</p>
+            ))}
+          </div>
+          <hr />
+        </div>
+      );
+    }
+
+    // content = [[1,2],[3,4],[5,6]]
+    // item = [1,2], [3,4], [5,6]
+    return (
+      <>
+        {headJSX}
+        <ul>
+          {body.map((item, i) => (
+            <li key={i} id="row">
+              {item.map((value, i) => (
+                <p key={i}>{value}</p>
+              ))}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  };
+
+  // render 'Content' depending on if there's a tab prop if not fallback to default
+  let renderContent;
+  if (activeTab !== null && activeTab >= 0) {
+    const currentTab = tabs[activeTab];
+
+    const tabContentHead = currentTab.contentHead || [];
+    const tabContent = currentTab.content;
+
+    renderContent = <Content head={tabContentHead} body={tabContent} />;
+  } else {
+    renderContent = <Content head={contentHead} body={content} />;
+  }
 
   return (
     <div className="component-container" id={id || null}>
-      {isCustom.head ? null : (
-        <div id="head">
-          <div>
-            {strNav && <p>{title}</p>}
-            {btnNav &&
-              title.map((btn, i) => {
-                return (
-                  <button key={i} id="type-text">
-                    {btn}
-                  </button>
-                );
-              })}
-          </div>
-        </div>
-      )}
-      <div id="content">
-        {isCustom.content ? (
-          children
-        ) : (
-          <>
-            {contentHead.length > 0 && (
-              <div id="content-head">
-                <div id="row">
-                  {contentHead.map((value, i) => (
-                    <p key={i}>{value}</p>
-                  ))}
-                </div>
-                <hr />
-              </div>
-            )}
-
-            <ul>
-              {content.map((item, i) => (
-                <li key={i} id="row">
-                  {item.map((value, i) => (
-                    <p key={i}>{value}</p>
-                  ))}
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-      </div>
+      {hasCustom.head ? null : <Head />}
+      <div id="content">{hasCustom.content ? children : renderContent}</div>
     </div>
   );
 }
