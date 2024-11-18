85c24e3fe8192213cfd3187ade96f50751042769
Mok <mok@moekm.com>
2024-11-18 17:25:07 +0300

    New branding + FAQ minor fixes

--- src/components/landing-page/static/FAQ.js
+++ src/components/landing-page/static/FAQ.js
@@ -5,19 +5,19 @@ const FAQ = [
   ],
   [
     "How is there unlimited builds, bandwidth, etc.. in the package?",
-    "They're possible because you’re in the driver’s seat, hosting everything on your own server. There’s no cap from us.\n Performance and limits now depend on your hosting plan, the better the server, the faster it will run!",
+    "They're possible because you’re in the driver’s seat, hosting everything on your own server. There’s no cap from us.\n Performance and limits now depend on your hosting plan.",
   ],
   [
     "Where can I deploy/use this site?",
-    "You can host this tool on any server that supports HTML & CSS. Since this is a demo project showcasing my work, you won’t need anything fancy—just a place that can handle some basic web files.",
+    "You can host this site on any host that supports HTML & CSS. Since this is just a demo project, you won’t need anything fancy just a host that can handle some basic web files.",
   ],
   [
     "Why did you build this?",
-    "Because I love creating stuff like this! It’s a fun way to practice my skills and because it's also fun :)",
+    "Because I love creating stuff like this! and It’s a fun way to practice my skills :)",
   ],
   [
     "Is this a legit FAQ section?",
-    "Well... sort of. Since there’s no actual product to deliver, just a front-end site. This FAQ is more about explaining the project. But , it’s honest and informative, and that counts for something, right?",
+    "Well... not really. Since it's just a front-end site with no backend (yet), there's no actual product to deliver.",
   ],
 ];
 
